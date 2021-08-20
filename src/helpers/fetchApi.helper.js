b6564e40aa90616e81eb70b959fa9356fc9c6b3e
Candra Rahmawan <candraa.rahmawan@gmail.com>
2021-08-20 08:46:31 +0700

    chore: remove proxy using dev env directly

--- src/helpers/fetchApi.helper.js
+++ src/helpers/fetchApi.helper.js
@@ -3,15 +3,13 @@ import qs from 'qs';
 import { IS_OK, IS_UNAUTHORIZED } from 'constants/httpStatus.constant';
 import { getUser } from 'helpers/auth.helper';
 
-const API_URL = process.env.NODE_ENV === 'development' ? '' : process.env.REACT_APP_API;
-
 const isMethodGET = (method) => method === 'GET';
 
 const bindUrlWithParam = (url, method, data) =>
   format(url, data) + (isMethodGET(method) ? `?${qs.stringify(data)}` : '');
 
 export const fetchApiClient = (url, method, data) =>
-  fetch(bindUrlWithParam(API_URL + url, method, data), {
+  fetch(bindUrlWithParam(process.env.REACT_APP_API + url, method, data), {
     method,
     headers: {
       api_token: getUser()?.token,
