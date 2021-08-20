9768ed35040aa45abaab23156fa0ae431e1ba757
Candra Rahmawan <candraa.rahmawan@gmail.com>
2021-08-20 19:44:48 +0700

    chore: add hoc style on check balanced container

--- src/apps/containers/Dashboard/CheckBalanced.container.jsx
+++ src/apps/containers/Dashboard/CheckBalanced.container.jsx
@@ -1,13 +1,18 @@
 import { func, object } from 'prop-types';
+import { Grid } from '@material-ui/core';
+import { withStyles } from '@material-ui/core/styles';
+
+import styles from './style';
 
 const CheckBalancedContainer = () => {
-  return <div>Check Balanced</div>;
+  return <Grid>Check Balanced</Grid>;
 };
 
 CheckBalancedContainer.propTypes = {
   t: func.isRequired,
+  classes: object.isRequired,
   history: object.isRequired,
   location: object.isRequired,
 };
 
-export default CheckBalancedContainer;
+export default withStyles(styles)(CheckBalancedContainer);
