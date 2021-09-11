cced257c358c60802b46d565b9426603dda071e5
yuniikrm <yuniikrima.94@gmail.com>
2021-09-11 20:16:20 +0700

    Add build

---
+++ build/static/js/37.1ae2bc20.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpmaqshaf-pkr-web"]=this["webpackJsonpmaqshaf-pkr-web"]||[]).push([[37],{440:function(e,t,s){"use strict";s.r(t);var r=s(423),a=s(477),n=s(4),i=function(e){var t=e.showAlert,s=e.setShowAlert,i=e.text,o=e.severity;return Object(n.jsx)(a.a,{open:t,autoHideDuration:2e3,onClose:function(){return s(!1)},children:Object(n.jsx)(r.a,{severity:o,variant:"filled",children:i})})};i.defaultProps={severity:"success"},t.default=i}}]);
+//# sourceMappingURL=37.1ae2bc20.chunk.js.map
